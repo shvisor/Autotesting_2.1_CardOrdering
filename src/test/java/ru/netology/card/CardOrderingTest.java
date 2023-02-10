9416ece53dfca8c591790b0a2bcf4d74c9a9a737
Max Shvagerus <mshvagerus@gmail.com>
2023-02-10 21:59:03 +0300

    add first test

--- src/test/java/ru/netology/card/CardOrderingTest.java
+++ src/test/java/ru/netology/card/CardOrderingTest.java
@@ -1,28 +1,29 @@
 package ru.netology.card;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CardOrderingTest {
     private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver.win.chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
     @BeforeEach
     void setUp() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:7777");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -32,7 +33,15 @@ public class CardOrderingTest {
     }
 
     @Test
-    void shouldCardForm() throws InterruptedException {
-        driver.get("http://localhost:9999");
+    void shouldCardForm() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ямщиков Максим");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79651234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
     }
 }
