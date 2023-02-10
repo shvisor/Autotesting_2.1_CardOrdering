06398c2a76e53c42a928fba6cd872886d9f1240e
Max Shvagerus <mshvagerus@gmail.com>
2023-02-11 00:42:47 +0300

    changing BeforeEach

--- src/test/java/ru/netology/card/CardOrderingTest.java
+++ src/test/java/ru/netology/card/CardOrderingTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,17 +14,17 @@ public class CardOrderingTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
-        driver.get("http://localhost:9999");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -61,6 +62,19 @@ public class CardOrderingTest {
     @Test
     void shouldCardFormWithSpace() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Смит Вессон Джон"); // Двойная фамилия через пробел
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79651234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCardFormJustName() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Джон"); // Только имя без фамилии
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79651234567");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
