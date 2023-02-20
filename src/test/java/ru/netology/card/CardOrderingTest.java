d931d7edd5f3ec9a6a9efba5738e3f4f4cc29ee3
Max Shvagerus <mshvagerus@gmail.com>
2023-02-20 05:10:29 +0300

    changing host

--- src/test/java/ru/netology/card/CardOrderingTest.java
+++ src/test/java/ru/netology/card/CardOrderingTest.java
@@ -24,7 +24,7 @@ public class CardOrderingTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:7777");
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
